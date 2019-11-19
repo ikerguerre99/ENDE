d5a3738183ef91ce57000c7ccd5e60fec80e0527
Iker Guerrero <Iker@DESKTOP-REIGI6U>
2019-11-19 13:13:53 +0100

    menu 4 opciones

---
+++ src/menuakGit/menuak.java
@@ -0,0 +1,13 @@
+package menuakGit;
+
+public class menuak {
+
+	public static void main(String[] args) {
+		System.out.println("----MENUA----\n"
+				+ "0-irten\n"
+				+ "1- arrainak zerrendatu\n"
+				+ "3- janari motak zerrendatu\n"
+				+ "4- kaloriak aldatu\n");
+	}
+
+}
