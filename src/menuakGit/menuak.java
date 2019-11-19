015aa69f2c2a6c4bc7e8a79edd4a95e801f5ab09
Iker Guerrero <Iker@DESKTOP-REIGI6U>
2019-11-19 13:25:49 +0100

    menu 4 implementado

--- src/menuakGit/menuak.java
+++ src/menuakGit/menuak.java
@@ -1,13 +1,36 @@
 package menuakGit;
 
+import java.util.Scanner;
+
 public class menuak {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int aukera;
+		
 		System.out.println("----MENUA----\n"
-				+ "0-irten\n"
-				+ "1- arrainak zerrendatu\n"
-				+ "3- janari motak zerrendatu\n"
-				+ "4- kaloriak aldatu\n");
+				+ "0- gehiketa\n"
+				+ "1- kenketa\n"
+				+ "3- biderketa\n"
+				+ "4- zatiketa\n");
+		
+		aukera = Integer.parseInt(scan.nextLine());
+		
+		switch (aukera) {
+		case 1:
+			System.out.println("Gehiketa aukeratu duzu");
+			break;
+		case 2:
+			System.out.println("Kenketa aukeratu duzu");
+			break;
+		case 3:
+			System.out.println("Biderketa aukeratu duzu");
+			break;
+		case 4:
+			System.out.println("Zatiketa aukeratu duzu");
+			break;
+		
 	}
 
+	}
 }
